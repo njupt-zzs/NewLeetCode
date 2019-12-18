fce11b31d8b82af17e89884189ea114f34867d7f
njupt-zzs <1442998796@qq.com>
2019-12-18 20:14:40 +0800

    再次增加了文件

---
+++ Linked/src/com/gczx/juc/Foo001.java
@@ -0,0 +1,11 @@
+package com.gczx.juc;
+
+/**
+ * @authoor zzs
+ * @create 2019-12-18 20:12
+ */
+public class Foo001 {
+    public static void main(String[] args) {
+        System.out.println("憨憨来喽");
+    }
+}
