8890d272565a3edc6825c291a352dd2e44d2fb4c
njupt-zzs <1442998796@qq.com>
2019-12-18 20:09:51 +0800

    增加了的文件，来验证自己的猜想

---
+++ Linked/src/com/gczx/juc/Foo.java
@@ -0,0 +1,11 @@
+package com.gczx.juc;
+
+/**
+ * @authoor zzs
+ * @create 2019-12-18 20:06
+ */
+public class Foo {
+    public static void main(String[] args) {
+        System.out.println("hello world!");
+    }
+}
